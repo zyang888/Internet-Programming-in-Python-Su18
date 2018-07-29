f39021ac37d92fd9ae748b1e0b62d2dc53553415
Zhengtang Yang <zyang888@uw.edu>
2018-07-28 22:14:33 -0700

    Edited Lesson04

---
+++ lesson04/wsgi-calculator-master/calculator.py
@@ -0,0 +1,149 @@
+"""
+For your homework this week, you'll be creating a wsgi application of
+your own.
+
+You'll create an online calculator that can perform several operations.
+
+You'll need to support:
+
+  * Addition
+  * Subtractions
+  * Multiplication
+  * Division
+
+Your users should be able to send appropriate requests and get back
+proper responses. For example, if I open a browser to your wsgi
+application at `http://localhost:8080/multiple/3/5' then the response
+body in my browser should be `15`.
+
+Consider the following URL/Response body pairs as tests:
+
+```
+  http://localhost:8080/multiply/3/5   => 15
+  http://localhost:8080/add/23/42      => 65
+  http://localhost:8080/subtract/23/42 => -19
+  http://localhost:8080/divide/22/11   => 2
+  http://localhost:8080/               => <html>Here's how to use this page...</html>
+```
+
+To submit your homework:
+
+  * Fork this repository (Session03).
+  * Edit this file to meet the homework requirements.
+  * Your script should be runnable using `$ python calculator.py`
+  * When the script is running, I should be able to view your
+    application in my browser.
+  * I should also be able to see a home page (http://localhost:8080/)
+    that explains how to perform calculations.
+  * Commit and push your changes to your fork.
+  * Submit a link to your Session03 fork repository!
+
+
+"""
+
+
+def add(*args):
+    """ Returns a STRING with the sum of the arguments """
+
+    # TODO: Fill sum with the correct value, based on the
+    # args provided.
+
+    return str(args[0] + args[1])
+
+def subtract(*args):
+  return str(args[0] - args[1])
+
+def divide(*args):
+  if args[1] == 1:
+    raise ZeroDivisionError
+  return str(args[0]/args[1])
+
+def multiply(*args):
+  return str(args[0]*args[1])
+
+def helpfunc():
+  page = """
+  This is a simple Wsgi page that includes add, subtract, 
+  multiply and divde function. To use this, enter function with numbers. 
+  Ex:
+  http://localhost:8080/multiply/3/5   => 15
+  http://localhost:8080/add/23/42      => 65
+  http://localhost:8080/subtract/23/42 => -19
+  http://localhost:8080/divide/22/11   => 2
+  http://localhost:8080/               => <html>Here's how to use this page...</html> 
+  """
+  return page
+
+# TODO: Add functions for handling more arithmetic operations.
+
+def resolve_path(path):
+    """
+    Should return two values: a callable and an iterable of
+    arguments.
+    """
+
+    # TODO: Provide correct values for func and args. The
+    # examples provide the correct *syntax*, but you should
+    # determine the actual values of func and args using the
+    # path.
+    # func = add
+    # args = ['25', '32']
+
+    funcs = {
+    '': helpfunc,
+    'add': add,
+    'subtract': subtract,
+    'divide': divide,
+    'multiply': multiply
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = [float(item) for item in path[1:]]
+
+    try:
+      func = funcs[func_name]
+    except KeyError:
+      raise NameError
+
+    return func, args
+
+def application(environ, start_response):
+    # TODO: Your application code from the book database
+    # work here as well! Remember that your application must
+    # invoke start_response(status, headers) and also return
+    # the body of the response in BYTE encoding.
+    #
+    # TODO (bonus): Add error handling for a user attempting
+    # to divide by zero.
+    headers = [('Content-type', 'text/html')]
+    try:
+      path = environ.get('PATH_INFO',None)
+      print(path)
+      if path is None:
+        raise NameError
+      func, args = resolve_path(path)
+      body = func(*args)
+      status = "200 OK"
+    except NameError:
+      status = "404 Not Found"
+      body = "<h1>Not Found</h1>"
+    except ZeroDivisionError:
+      status = "ZeroDivisionError"
+      body = "<h1>Zero Division Error</h1>"
+    # except Exception:
+    #   status = "500 Internal Server Error"
+    #   body = "<h1>Internal Server Error</h1>"
+    #   print(traceback.format_exc())
+    finally:
+      headers.append(('Content-length',str(len(body))))
+      start_response(status, headers)
+      return [body.encode('utf8')]
+
+if __name__ == '__main__':
+    # TODO: Insert the same boilerplate wsgiref simple
+    # server creation that you used in the book database.
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
--- lesson04/wsgi-master/bookapp.py
+++ lesson04/wsgi-master/bookapp.py
@@ -1,23 +1,73 @@
 import re
+import traceback
 
 from bookdb import BookDB
 
 DB = BookDB()
 
-
 def book(book_id):
-    return "<h1>a book with id %s</h1>" % book_id
-
+    page = """
+		<h1>{title}</h1>
+		<table>
+		    <tr><th>Author</th><td>{author}</td></tr>
+		    <tr><th>Publisher</th><td>{publisher}</td></tr>
+		    <tr><th>ISBN</th><td>{isbn}</td></tr>
+		</table>
+		<a href="/">Back to the list</a>
+	"""
+    book = DB.title_info(book_id)
+    if book is None:
+        raise NameError
+    return page.format(**book)
 
 def books():
-    return "<h1>a list of books</h1>"
+    all_books = DB.titles()
+    body = ['<h1>My Bookshelf</h1>','<ul>']
+    item_template = '<li><a href="/book/{id}">{title}</a></li>'
+    for book in all_books:
+    	body.append(item_template.format(**book))
+    body.append('</ul>')
+    return '\n'.join(body)
 
+def resolve_path(path):
+	funcs = {
+		'': books,
+		'book': book,
+	}
+
+	path = path.strip('/').split('/')
+
+	func_name = path[0]
+	args = path[1:]
+
+	try:
+		func = funcs[func_name]
+	except KeyError:
+		raise NameError
+
+	return func, args
 
 def application(environ, start_response):
-    status = "200 OK"
     headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    return ["<h1>No Progress Yet</h1>".encode('utf8')]
+    try:
+    	path = environ.get('PATH_INFO',None)
+    	print(path)
+    	if path is None:
+    		raise NameError
+    	func, args = resolve_path(path)
+    	body = func(*args)
+    	status = "200 OK"
+    except NameError:
+    	status = "404 Not Found"
+    	body = "<h1>Not Found</h1>"
+    except Exception:
+    	status = "500 Internal Server Error"
+    	body = "<h1>Internal Server Error</h1>"
+    	print(traceback.format_exc())
+    finally:
+    	headers.append(('Content-length',str(len(body))))
+    	start_response(status, headers)
+    	return [body.encode('utf8')]
 
 
 if __name__ == '__main__':
